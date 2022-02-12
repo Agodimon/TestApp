ed2ba3bde63f92c76948cc09a3b01f057a57fea4
dmitryagoshkov <agodimon@mail.ru>
2022-02-12 13:58:36 +0500

    Initial commit

---
+++ app/src/test/java/com/example/testapp/EmailValidatorTest.kt
@@ -0,0 +1,50 @@
+package com.example.testapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class EmailValidatorTest {
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com"))
+    }
+
+    @Test
+    fun emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.co.uk"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email..com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("@email.com"))
+    }
+
+    @Test
+    fun emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""))
+    }
+
+    @Test
+    fun emailValidator_NullEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(null))
+    }
+
+
+
+}
