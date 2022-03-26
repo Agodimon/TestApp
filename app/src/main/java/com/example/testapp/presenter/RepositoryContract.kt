d14d23719aa999a809ed0f5dece11d7aff9bcdd1
dmitryagoshkov <agodimon@mail.ru>
2022-03-26 18:07:04 +0500

    выполнил тестирование SearchViewModel.kt

--- app/src/main/java/com/example/testapp/presenter/RepositoryContract.kt
+++ app/src/main/java/com/example/testapp/presenter/RepositoryContract.kt
@@ -4,14 +4,14 @@ import com.example.testapp.model.SearchResponse
 import com.example.testapp.repository.RepositoryCallback
 import io.reactivex.Observable
 
-internal interface RepositoryContract {
+interface RepositoryContract {
+
     fun searchGithub(
         query: String,
         callback: RepositoryCallback
     )
+
     fun searchGithub(
         query: String
     ): Observable<SearchResponse>
-
-
 }
--- app/src/main/java/com/example/testapp/view/search/MainActivity.kt
+++ app/src/main/java/com/example/testapp/view/search/MainActivity.kt
@@ -6,7 +6,9 @@ import android.view.inputmethod.EditorInfo
 import android.widget.*
 import android.widget.TextView.OnEditorActionListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
+import com.example.testapp.BuildConfig
 import com.example.testapp.R
 import com.example.testapp.model.SearchResult
 import com.example.testapp.presenter.search.PresenterSearchContract
@@ -14,7 +16,9 @@ import com.example.testapp.presenter.search.SearchPresenter
 import com.example.testapp.repository.GitHubApi
 import com.example.testapp.repository.GitHubRepository
 import com.example.testapp.presenter.RepositoryContract
+import com.example.testapp.repository.FakeGitHubRepository
 import com.example.testapp.view.details.DetailsActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -24,17 +28,49 @@ import java.util.*
 class MainActivity : AppCompatActivity(), ViewSearchContract {
 
     private val adapter = SearchResultAdapter()
-    private val presenter: PresenterSearchContract = SearchPresenter(this, createRepository())
+    private val viewModel: SearchViewModel by lazy {
+        ViewModelProvider(this).get(SearchViewModel::class.java)
+    }
     private var totalCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUI()
+        viewModel.subscribeToLiveData().observe(this) { onStateChange(it) }
+    }
+
+    private fun onStateChange(screenState: ScreenState) {
+        when (screenState) {
+            is ScreenState.Working -> {
+                val searchResponse = screenState.searchResponse
+                val totalCount = searchResponse.totalCount
+                progressBar.visibility = View.GONE
+                with(totalCountTextView) {
+                    visibility = View.VISIBLE
+                    text =
+                        String.format(
+                            Locale.getDefault(),
+                            getString(R.string.results_count),
+                            totalCount
+                        )
+                }
+
+                this.totalCount = totalCount!!
+                adapter.updateResults(searchResponse.searchResults!!)
+            }
+            is ScreenState.Loading -> {
+                progressBar.visibility = View.VISIBLE
+            }
+            is ScreenState.Error -> {
+                progressBar.visibility = View.GONE
+                Toast.makeText(this, screenState.error.message, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun setUI() {
-        findViewById<Button>(R.id.toDetailsActivityButton).setOnClickListener {
+        toDetailsActivityButton.setOnClickListener {
             startActivity(DetailsActivity.getIntent(this, totalCount))
         }
         setQueryListener()
@@ -42,16 +78,16 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
     }
 
     private fun setRecyclerView() {
-        findViewById<RecyclerView>(R.id.recyclerView).setHasFixedSize(true)
-        findViewById<RecyclerView>(R.id.recyclerView).adapter = adapter
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
     }
 
     private fun setQueryListener() {
-        findViewById<EditText>(R.id.searchEditText).setOnEditorActionListener(OnEditorActionListener { _, actionId, _ ->
+        searchEditText.setOnEditorActionListener(OnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                val query = findViewById<EditText>(R.id.searchEditText).text.toString()
+                val query = searchEditText.text.toString()
                 if (query.isNotBlank()) {
-                    presenter.searchGitHub(query)
+                    viewModel.searchGitHub(query)
                     return@OnEditorActionListener true
                 } else {
                     Toast.makeText(
@@ -67,7 +103,11 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
     }
 
     private fun createRepository(): RepositoryContract {
-        return  GitHubRepository(createRetrofit().create(GitHubApi::class.java))
+        return if (BuildConfig.TYPE == FAKE) {
+            FakeGitHubRepository()
+        } else {
+            GitHubRepository(createRetrofit().create(GitHubApi::class.java))
+        }
     }
 
     private fun createRetrofit(): Retrofit {
@@ -82,9 +122,10 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
         searchResults: List<SearchResult>,
         totalCount: Int
     ) {
-        with(findViewById<TextView>(R.id.totalCountTextView)) {
+        with(totalCountTextView) {
             visibility = View.VISIBLE
-            text = String.format(Locale.getDefault(), getString(R.string.results_count), totalCount)
+            text =
+                String.format(Locale.getDefault(), getString(R.string.results_count), totalCount)
         }
 
         this.totalCount = totalCount
@@ -101,16 +142,14 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
 
     override fun displayLoading(show: Boolean) {
         if (show) {
-            findViewById<ProgressBar>(R.id.progressBar).visibility = View.VISIBLE
+            progressBar.visibility = View.VISIBLE
         } else {
-            findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
+            progressBar.visibility = View.GONE
         }
     }
 
-
-        companion object {
-            const val BASE_URL = "https://api.github.com"
-            const val FAKE = "FAKE"
-        }
-
+    companion object {
+        const val BASE_URL = "https://api.github.com"
+        const val FAKE = "FAKE"
+    }
 }
